194821995b6e46771b80dc504fbd7aaa3968d94f
Gennadiy Tsarenkov <gtsarenkov@users.noreply.github.com>
2021-01-18 15:10:05 +0300

    Fixed bug under Windows environment preventing from start.
    NOTE: Not tested under Linux yet.

--- src/main/java/de/flapdoodle/embed/process/store/PostgresArtifactStoreBuilder.java
+++ src/main/java/de/flapdoodle/embed/process/store/PostgresArtifactStoreBuilder.java
@@ -1,6 +1,8 @@
 package de.flapdoodle.embed.process.store;
 
+import de.flapdoodle.embed.process.config.store.ImmutableDownloadConfig;
 import de.flapdoodle.embed.process.extract.UUIDTempNaming;
+import de.flapdoodle.embed.process.io.directories.Directory;
 import ru.yandex.qatools.embed.postgresql.Command;
 import ru.yandex.qatools.embed.postgresql.config.PostgresDownloadConfigBuilder;
 import ru.yandex.qatools.embed.postgresql.ext.SubdirTempDir;
@@ -26,9 +28,18 @@ public class PostgresArtifactStoreBuilder {
         return this;
     }
 
+    public PostgresArtifactStoreBuilder tempDirFactory (Directory cachedDir) {
+        builder.tempDirFactory (cachedDir);
+        return this;
+    }
+
+    public PostgresArtifactStoreBuilder downloadConfig (ImmutableDownloadConfig config) {
+        builder.downloadConfig (config);
+        return this;
+    }
+
     public PostgresArtifactStore build(Function<ImmutableArtifactStore.Builder, ImmutableArtifactStore> builder) {
         final ImmutableArtifactStore artifactStore = builder.apply(this.builder);
         return new CachedPostgresArtifactStore(artifactStore.downloadConfig(), artifactStore.tempDirFactory(), artifactStore.executableNaming(), artifactStore.downloader());
     }
-
 }
