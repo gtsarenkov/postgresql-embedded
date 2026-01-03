7b0c94244e5bd8cc774965192072d7b1c756b13e
Gennadiy Tsarenkov <gtsarenkov@users.noreplay.github.com>
2026-01-03 18:42:04 +0100

    Workaround a bug propagating of error stream redirect into ProcessBuilder.

--- src/main/java/ru/yandex/qatools/embed/postgresql/AbstractPGProcess.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/AbstractPGProcess.java
@@ -21,6 +21,13 @@ public abstract class AbstractPGProcess<E extends Executable<PostgresConfig, P>,
 
     @Override
     protected void onBeforeProcessStart(ProcessBuilder processBuilder, PostgresConfig config, RuntimeConfig runtimeConfig) {
+        /*
+         * begin-of-bugix
+         * There is a bug in de.flapdoodle.embed.process.runtime.ProcessControl.newProcessBuilder(java.util.List<java.lang.String>, java.util.Map<java.lang.String,java.lang.String>, boolean)
+         * redirectErrorStream value not propagated to ProcessBuilder, thus make it here.
+         */
+        processBuilder.redirectErrorStream(true);
+        // enf-of-bugfix
         if (config.credentials() != null) {
             processBuilder.environment().put("PGUSER", config.credentials().username());
             processBuilder.environment().put("PGPASSWORD", config.credentials().password());
